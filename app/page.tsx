bf1bd9cbfa75d94038285044f17305b84becc5c1
Andriy Romanov <mgm.agro04@gmail.com>
2025-07-02 17:20:04 +0300

    edit team

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ const tasks = [
 
 export default function HomePage() {
   return (
-    <section className={styles.main}>
+    <section className={styles.hero}>
       <div className={styles.content}>
         <div className={styles.textBlock}>
           <h1 className={styles.heading}>«Do It Now» by Brian Tracy</h1>
@@ -37,7 +37,7 @@ export default function HomePage() {
           <div className={styles.imageGroup}>
             <Image
               src="/book.webp"
-              alt="Книга"
+              alt="book Do it Now"
               width={300}
               height={400}
               sizes="(max-width: 480px) 200px,
@@ -48,7 +48,7 @@ export default function HomePage() {
             />
             <Image
               src="/frogs/frog_with_red_legs.webp"
-              alt="Жабеня"
+              alt="frog"
               width={120}
               height={120}
               className={styles.imageFrog}
